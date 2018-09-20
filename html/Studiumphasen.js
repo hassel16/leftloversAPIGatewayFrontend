ad7c6183e11c736be48a8a5f5589ba350c993434
daniel.hasselwander@nttdata.com <daniel.hasselwander@nttdata.com>
2018-09-21 01:48:37 +0200

    persistent jahrgan

--- html/Studiumphasen.js
+++ html/Studiumphasen.js
@@ -46,153 +46,164 @@ class Phase {
         return (this.phasen + " " + this.startDatum + " " + this.endDatum + "");
     }
 }
-    
 
-    let arrayJahrgang = getFromStorage();
-    let arrayPhase = new Array();
 
-    function jahrgangHinzufuegen() {
-        if (auslesenUndJahrgangErzeugen() != false) {
-            let jahrgangneu = auslesenUndJahrgangErzeugen();
-            jahrgangneu.studienphasen = arrayPhase;
-            arrayPhase = new Array();
-            arrayJahrgang.push(jahrgangneu);
-            saveInStorage();
-            resetFormJahrgang();
-            generiereUebersichtstabelle();
-            generiereStringForHTMLJahrgang();
-        } else {
-            alert("Fehlerhafte Eingabe! Bitte überprüfen");
-        }
-    }
+let arrayJahrgang;
+getFromStorage();
+let arrayPhase = new Array();
 
-    function saveInStorage() {
-         $.post( "https://studentenuebersicht.herokuapp.com/storage/studiengang",arrayJahrgang );
-        //$.post("http://127.0.0.1:8080/storage/studiengang", arrayJahrgang);
-    }
-
-    function getFromStorage() {
-        jQuery.get("https://studentenuebersicht.herokuapp.com/storage/studiengang", function (data) {
-        alert(data);    
-        return data;
-        });
-    }
-
-    function auslesenUndJahrgangErzeugen() {
-        let txtfeldStudiengang = $("#studiengang").val();
-        let txtfeldJahrgang = $("#jahrgang").val();
-        let boxSchule = $("input[name='hochschule']:checked").val();
-
-        if (txtfeldStudiengang == "" || txtfeldJahrgang == "" || boxSchule == undefined) {
-            return false;
-        }
-
-        let jahrgangneu = new Jahrgangneu(txtfeldStudiengang, txtfeldJahrgang, boxSchule);
-        return jahrgangneu;
-    }
-    function resetFormJahrgang() {
-        $("#studiengang").val("");
-        $("#jahrgang").val("");
-        $("input[name='hochschule']:checked").prop("checked", false);
-        $("#UebersichtPhase").html("");
-    }
-
-    function loescheAusarrayJahrgang(number) {
-        arrayJahrgang.splice(number, 1);
+function jahrgangHinzufuegen() {
+    if (auslesenUndJahrgangErzeugen() != false) {
+        let jahrgangneu = auslesenUndJahrgangErzeugen();
+        jahrgangneu.studienphasen = arrayPhase;
+        arrayPhase = new Array();
+        arrayJahrgang.push(jahrgangneu);
+        saveInStorage();
+        resetFormJahrgang();
+        generiereUebersichtstabelle();
         generiereStringForHTMLJahrgang();
+    } else {
+        alert("Fehlerhafte Eingabe! Bitte überprüfen");
+    }
+}
 
+function saveInStorage() {
+    //$.post( "https://studentenuebersicht.herokuapp.com/storage/studiengang",arrayJahrgang );
+    $.ajax({
+        url: 'http://127.0.0.1:8080/storage/studiengang',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+        },
+        data: JSON.stringify(arrayJahrgang)
+    });
+    //$.post("http://127.0.0.1:8080/storage/studiengang", JSON.stringify(arrayJahrgang));
+}
+
+function getFromStorage() {
+    jQuery.get("http://127.0.0.1:8080/storage/studiengang", function (data) {
+        arrayJahrgang = data;
+        generiereUebersichtstabelle();
+        generiereStringForHTMLJahrgang();
+    });
+}
+
+function auslesenUndJahrgangErzeugen() {
+    let txtfeldStudiengang = $("#studiengang").val();
+    let txtfeldJahrgang = $("#jahrgang").val();
+    let boxSchule = $("input[name='hochschule']:checked").val();
+
+    if (txtfeldStudiengang == "" || txtfeldJahrgang == "" || boxSchule == undefined) {
+        return false;
     }
 
-    function generiereStringForHTMLJahrgang() {
+    let jahrgangneu = new Jahrgangneu(txtfeldStudiengang, txtfeldJahrgang, boxSchule);
+    return jahrgangneu;
+}
+function resetFormJahrgang() {
+    $("#studiengang").val("");
+    $("#jahrgang").val("");
+    $("input[name='hochschule']:checked").prop("checked", false);
+    $("#UebersichtPhase").html("");
+}
 
+function loescheAusarrayJahrgang(number) {
+    arrayJahrgang.splice(number, 1);
+    generiereStringForHTMLJahrgang();
+
+}
+
+function generiereStringForHTMLJahrgang() {
+
+}
+
+function bearbeitenAusarrayJahrgang(number) {
+    let jahrgangneu = new Jahrgangneu();
+    jahrgangneu = arrayJahrgang[number];
+
+    $("#studiengang").val(jahrgangneu.studiengang);
+    $("#jahrgang").val(jahrgangneu.jahrgang);
+    $("input[name='hochschule'][value='" + jahrgangneu.hochschule + "']").prop("checked", true);
+
+    $("#buttons").html(" <Button onclick='bearbeitungSpeichernJahrgang(" + number + ")'>Speichern</Button>");
+}
+
+function setButtons(value) {
+    $("input[name='hochschule']:checked").prop("checked", false);
+    $(`#${value}`).prop("checked", true);
+}
+
+function bearbeitungSpeichernJahrgang(number) {
+    if (auslesenUndJahrgangErzeugen() != false) {
+        let jahrgangneu = auslesenUndJahrgangErzeugen();
+        arrayJahrgang[number] = jahrgangneu;
+        resetFormJahrgang();
+        $("#buttons").html("<button onclick='phaseHinzufuegen' class='btn btn-primary'>Phase hinzufügen</button>" + " " + "<button onclick='jahrgangHinzufuegen()' class='btn btn-primary'>Jahrgang hinzufügen</button>");
+        generiereStringForHTMLJahrgang();
+    } else {
+        alert("Fehlerhafte Eingabe! Bitte überprüfen!");
     }
-
-    function bearbeitenAusarrayJahrgang(number) {
-        let jahrgangneu = new Jahrgangneu();
-        jahrgangneu = arrayJahrgang[number];
-
-        $("#studiengang").val(jahrgangneu.studiengang);
-        $("#jahrgang").val(jahrgangneu.jahrgang);
-        $("input[name='hochschule'][value='" + jahrgangneu.hochschule + "']").prop("checked", true);
-
-        $("#buttons").html(" <Button onclick='bearbeitungSpeichernJahrgang(" + number + ")'>Speichern</Button>");
-    }
-
-    function setButtons(value) {
-        $("input[name='hochschule']:checked").prop("checked", false);
-        $(`#${value}`).prop("checked", true);
-    }
-
-    function bearbeitungSpeichernJahrgang(number) {
-        if (auslesenUndJahrgangErzeugen() != false) {
-            let jahrgangneu = auslesenUndJahrgangErzeugen();
-            arrayJahrgang[number] = jahrgangneu;
-            resetFormJahrgang();
-            $("#buttons").html("<button onclick='phaseHinzufuegen' class='btn btn-primary'>Phase hinzufügen</button>" + " " + "<button onclick='jahrgangHinzufuegen()' class='btn btn-primary'>Jahrgang hinzufügen</button>");
-            generiereStringForHTMLJahrgang();
-        } else {
-            alert("Fehlerhafte Eingabe! Bitte überprüfen!");
-        }
-    }
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    function phaseHinzufuegen() {
-        if (auslesenUndPhaseErzeugen() != false) {
-            let phase = auslesenUndPhaseErzeugen();
-            arrayPhase.push(phase);
-            resetFormPhase();
-            generiereStringForHTMLPhase();
-        } else {
-            alert("Fehlerhafte Eingabe! Bitte überprüfen!");
-        }
-    }
-
-    function auslesenUndPhaseErzeugen() {
-        let txtfeldPhase = $("#phasen").val();
-        let txtfeldStart = $("#startDatum").val();
-        let txtfeldEnde = $("#endDatum").val();
-
-        if (txtfeldPhase == "" || txtfeldStart == "" || txtfeldEnde == "") {
-            return false;
-        }
-
-        let phase = new Phase(txtfeldPhase, txtfeldStart, txtfeldEnde);
-        return phase;
-    }
-
-    function resetFormPhase() {
-        $("#formIDStudiphase").html(generiereNeueForm());
-    }
-
-    function bearbeitenAusarrayPhase(number) {
-        let phase = new Phase();
-        phase = arrayPhase[number];
-
-        $("#phasen").val(phase.phasen);
-        $("#startDatum").val(phase.startDatum);
-        $("#endDatum").val(phase.endDatum);
-
-        $("#buttons").html(" <Button onclick='bearbeitungSpeichernPhase(" + number + ")' class='btn btn-primary'>Speichern</Button>");
-    }
-
-    function bearbeitungSpeichernPhase(number) {
-        if (auslesenUndPhaseErzeugen() != false) {
-            let phase = auslesenUndPhaseErzeugen();
-            arrayPhase[number] = phase;
-            resetFormPhase();
-            $("#buttons").html("<button onclick='phaseHinzufuegen' class='btn btn-primary'>Phase hinzufügen</button>" + " " + "<button onclick='jahrgangHinzufuegen()' class='btn btn-primary'>Jahrgang hinzufügen</button>");
-            generiereStringForHTMLPhase();
-        } else {
-            alert("Fehlerhafte Eingabe! Bitte überprüfen!");
-        }
-    }
-    function loescheAusarrayPhase(number) {
-        arrayPhase.splice(number, 1);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function phaseHinzufuegen() {
+    if (auslesenUndPhaseErzeugen() != false) {
+        let phase = auslesenUndPhaseErzeugen();
+        arrayPhase.push(phase);
+        resetFormPhase();
         generiereStringForHTMLPhase();
+    } else {
+        alert("Fehlerhafte Eingabe! Bitte überprüfen!");
+    }
+}
 
+function auslesenUndPhaseErzeugen() {
+    let txtfeldPhase = $("#phasen").val();
+    let txtfeldStart = $("#startDatum").val();
+    let txtfeldEnde = $("#endDatum").val();
+
+    if (txtfeldPhase == "" || txtfeldStart == "" || txtfeldEnde == "") {
+        return false;
     }
 
-    function generiereStringForHTMLPhase() {
-        let stringForHtmlPhase = "<table class='table table-bordered'> \
+    let phase = new Phase(txtfeldPhase, txtfeldStart, txtfeldEnde);
+    return phase;
+}
+
+function resetFormPhase() {
+    $("#formIDStudiphase").html(generiereNeueForm());
+}
+
+function bearbeitenAusarrayPhase(number) {
+    let phase = new Phase();
+    phase = arrayPhase[number];
+
+    $("#phasen").val(phase.phasen);
+    $("#startDatum").val(phase.startDatum);
+    $("#endDatum").val(phase.endDatum);
+
+    $("#buttons").html(" <Button onclick='bearbeitungSpeichernPhase(" + number + ")' class='btn btn-primary'>Speichern</Button>");
+}
+
+function bearbeitungSpeichernPhase(number) {
+    if (auslesenUndPhaseErzeugen() != false) {
+        let phase = auslesenUndPhaseErzeugen();
+        arrayPhase[number] = phase;
+        resetFormPhase();
+        $("#buttons").html("<button onclick='phaseHinzufuegen' class='btn btn-primary'>Phase hinzufügen</button>" + " " + "<button onclick='jahrgangHinzufuegen()' class='btn btn-primary'>Jahrgang hinzufügen</button>");
+        generiereStringForHTMLPhase();
+    } else {
+        alert("Fehlerhafte Eingabe! Bitte überprüfen!");
+    }
+}
+function loescheAusarrayPhase(number) {
+    arrayPhase.splice(number, 1);
+    generiereStringForHTMLPhase();
+
+}
+
+function generiereStringForHTMLPhase() {
+    let stringForHtmlPhase = "<table class='table table-bordered'> \
     <thead>\
       <tr> \
         <th scope='col'>Phasenart</th>\
@@ -203,24 +214,24 @@ class Phase {
         <th scope='col'>Bearbeiten</th>\
         <th scope='col'>Löschen</th>\
         </thead><tbody>";
-        for (let i = 0; i < arrayPhase.length; i++) {
-            stringForHtmlPhase += "<tr>"
-            stringForHtmlPhase += "<td>" + arrayPhase[i].phasen + "</td>";
-            stringForHtmlPhase += "<td>" + arrayPhase[i].startDatum + "</td>";
-            stringForHtmlPhase += "<td>" + arrayPhase[i].endDatum + "</td>";
-            stringForHtmlPhase += "<td>" + arrayPhase[i].startKW + "</td>";
-            stringForHtmlPhase += "<td>" + arrayPhase[i].endKW + "</td>";
-            stringForHtmlPhase += "<td><Button class='btn btn-primary' onclick='bearbeitenAusarrayPhase(" + i + ")'>Bearbeiten</Button></td>";
-            stringForHtmlPhase += "<td><Button class='btn btn-primary' onclick='loescheAusarrayPhase(" + i + ")'>Loeschen</Button></td>";
-            //  <Button onclick='loescheAusarrayPhase(" + i + ")'>Loeschen</Button>" + "<Button onclick='bearbeitenAusarrayPhase(" + i + ")'>Bearbeiten</Button>" + "<br>";
-            stringForHtmlPhase += "</tr>"
-        }
-        stringForHtmlPhase += "</tbody>"
-
-        $("#UebersichtPhase").html(stringForHtmlPhase);
+    for (let i = 0; i < arrayPhase.length; i++) {
+        stringForHtmlPhase += "<tr>"
+        stringForHtmlPhase += "<td>" + arrayPhase[i].phasen + "</td>";
+        stringForHtmlPhase += "<td>" + arrayPhase[i].startDatum + "</td>";
+        stringForHtmlPhase += "<td>" + arrayPhase[i].endDatum + "</td>";
+        stringForHtmlPhase += "<td>" + arrayPhase[i].startKW + "</td>";
+        stringForHtmlPhase += "<td>" + arrayPhase[i].endKW + "</td>";
+        stringForHtmlPhase += "<td><Button class='btn btn-primary' onclick='bearbeitenAusarrayPhase(" + i + ")'>Bearbeiten</Button></td>";
+        stringForHtmlPhase += "<td><Button class='btn btn-primary' onclick='loescheAusarrayPhase(" + i + ")'>Loeschen</Button></td>";
+        //  <Button onclick='loescheAusarrayPhase(" + i + ")'>Loeschen</Button>" + "<Button onclick='bearbeitenAusarrayPhase(" + i + ")'>Bearbeiten</Button>" + "<br>";
+        stringForHtmlPhase += "</tr>"
     }
-    function generiereNeueForm() {
-        let htmlforForm = "<form class='form-inline'> \
+    stringForHtmlPhase += "</tbody>"
+
+    $("#UebersichtPhase").html(stringForHtmlPhase);
+}
+function generiereNeueForm() {
+    let htmlforForm = "<form class='form-inline'> \
 <select class='custom-select my-1 mr-sm-2' id='phasen' >\
   <option selected>Auswaehlen..</option>\
   <option value='Praxis'>Praxis</option>\
@@ -236,48 +247,48 @@ class Phase {
     <input type='date' id='endDatum' name='trip' value='' min='2015-01-01' max='2080-12-31' />\
   </div>\
 </fieldset>";
-        return htmlforForm;
-    }
+    return htmlforForm;
+}
 
 
-    function generiereUebersichtstabelle() {
-        let htmlAsString = "<table class='table table-bordered'> \
+function generiereUebersichtstabelle() {
+    let htmlAsString = "<table class='table table-bordered'> \
     <thead>\
       <tr> \
         <th scope='col'>HS</th>\
         <th scope='col'>SG</th>\
         <th scope='col'>JG</th>";
-        //Speicher größte Anzahl Phasen eines Studiengangs
-        let cacheSize = 0;
-        for (let counter = 0; counter < arrayJahrgang.length; counter++) {
-            if (cacheSize < arrayJahrgang[counter].studienphasen.length) {
-                cacheSize = arrayJahrgang[counter].studienphasen.length;
-            }
+    //Speicher größte Anzahl Phasen eines Studiengangs
+    let cacheSize = 0;
+    for (let counter = 0; counter < arrayJahrgang.length; counter++) {
+        if (cacheSize < arrayJahrgang[counter].studienphasen.length) {
+            cacheSize = arrayJahrgang[counter].studienphasen.length;
         }
-        for (let counterPhasen = 1; counterPhasen <= cacheSize; counterPhasen++) {
-            htmlAsString = htmlAsString + "<th scope='col'>" + counterPhasen + ". Phase</th>";
-        }
-        htmlAsString = htmlAsString + "</tr > </thead >";
-        //Bis hierhin ist die überschrift
-
-
-        htmlAsString = htmlAsString + "<tbody>";
-        for (let counter = 0; counter < arrayJahrgang.length; counter++) {
-            let jahrgang = new Jahrgangneu(arrayJahrgang[counter].studiengang, arrayJahrgang[counter].jahrgang, arrayJahrgang[counter].hochschule);
-            jahrgang.studienphasen = arrayJahrgang[counter].studienphasen;
-
-            //htmlAsString+="<tr>" gleich htmlAsString = htmlAsString +"<tr>"
-            htmlAsString += "<tr>";
-            htmlAsString += "<th>" + jahrgang.hochschule + "</th>";
-            htmlAsString += "<td>" + jahrgang.studiengang + "</td>";
-            htmlAsString += "<td>" + jahrgang.jahrgang + "</td>";
-            for (let counterPhasenFromJahrgang = 0; counterPhasenFromJahrgang < jahrgang.studienphasen.length; counterPhasenFromJahrgang++) {
-                htmlAsString += "<td>" + jahrgang.studienphasen[counterPhasenFromJahrgang].phasen + "<br>" + jahrgang.studienphasen[counterPhasenFromJahrgang].startDatum + " - " + jahrgang.studienphasen[counterPhasenFromJahrgang].endDatum + "</td>";
-            }
-            htmlAsString += "</tr>";
-        }
-        htmlAsString += "</tbody></table>";
-
-        $("#tableJahrgaenge").html(htmlAsString);
-
     }
+    for (let counterPhasen = 1; counterPhasen <= cacheSize; counterPhasen++) {
+        htmlAsString = htmlAsString + "<th scope='col'>" + counterPhasen + ". Phase</th>";
+    }
+    htmlAsString = htmlAsString + "</tr > </thead >";
+    //Bis hierhin ist die überschrift
+
+
+    htmlAsString = htmlAsString + "<tbody>";
+    for (let counter = 0; counter < arrayJahrgang.length; counter++) {
+        let jahrgang = new Jahrgangneu(arrayJahrgang[counter].studiengang, arrayJahrgang[counter].jahrgang, arrayJahrgang[counter].hochschule);
+        jahrgang.studienphasen = arrayJahrgang[counter].studienphasen;
+
+        //htmlAsString+="<tr>" gleich htmlAsString = htmlAsString +"<tr>"
+        htmlAsString += "<tr>";
+        htmlAsString += "<th>" + jahrgang.hochschule + "</th>";
+        htmlAsString += "<td>" + jahrgang.studiengang + "</td>";
+        htmlAsString += "<td>" + jahrgang.jahrgang + "</td>";
+        for (let counterPhasenFromJahrgang = 0; counterPhasenFromJahrgang < jahrgang.studienphasen.length; counterPhasenFromJahrgang++) {
+            htmlAsString += "<td>" + jahrgang.studienphasen[counterPhasenFromJahrgang].phasen + "<br>" + jahrgang.studienphasen[counterPhasenFromJahrgang].startDatum + " - " + jahrgang.studienphasen[counterPhasenFromJahrgang].endDatum + "</td>";
+        }
+        htmlAsString += "</tr>";
+    }
+    htmlAsString += "</tbody></table>";
+
+    $("#tableJahrgaenge").html(htmlAsString);
+
+}
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const jsonfile = require('jsonfile');
+const bodyParser = require('body-parser');
 const jsonSaveFile = './saveJSON.json';
+
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/html'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/html/Studentuebersicht.html'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
 });
+
+
 
 // Initialize the app.
 const server = app.listen(process.env.PORT || 8080, function () {
@@ -16,14 +24,15 @@ const server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", port);
 });
 
+const routerObj = readFromJSON();
+
 app.get('/storage/studiengang', function (req, res) {
-    res.statusCode(200).json(readFromJSON);
+    res.status(200).json(readFromJSON());
 });
 
 app.post('/storage/studiengang', function (req, res) {
-    res.statusCode(200).json(saveInJSON);
+    res.status(200).json(saveInJSON(req.body));
 });
-
 
 function saveInJSON(arrayFromStudiengang){
     jsonfile.writeFileSync(jsonSaveFile, arrayFromStudiengang)
@@ -32,3 +41,7 @@ function saveInJSON(arrayFromStudiengang){
 function readFromJSON(){
     return jsonfile.readFileSync(jsonSaveFile)
 }
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/Studentuebersicht.html'));
+});
